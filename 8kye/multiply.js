30a97fd9ccd31837cfcff7eb7ff8424efa23e999
antonyakobi <antonyakobi@yandex.by>
2024-10-26 21:22:13 +0300

    first commit

---
+++ 8kye/multiply.js
@@ -0,0 +1,22 @@
+// DESCRIPTION (ОПИСАНИЕ)
+// This code does not execute properly. Try to figure out why.
+// Этот код не выполняется правильно. Попробуйте выяснить, почему.
+// function multiply(a, b){
+//   a * b
+// }
+
+// TESTS 
+// assert.strictEqual(multiply(1,1), 1);
+// assert.strictEqual(multiply(2,1), 2);
+// assert.strictEqual(multiply(2,2), 4);
+// assert.strictEqual(multiply(3,5), 15); 
+// assert.strictEqual(multiply(5,0), 0);
+// assert.strictEqual(multiply(0,5), 0);
+// assert.strictEqual(multiply(0,0), 0); 
+
+// SOLUTION (РЕШЕНИЕ)
+function multiply(a, b){
+    return a * b
+}
+
+console.log(multiply(3, 5));
