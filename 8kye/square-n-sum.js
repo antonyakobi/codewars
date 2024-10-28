9b5a9b721719d316a4688432149106e47bfd8795
antonyakobi <antonyakobi@yandex.by>
2024-10-28 23:08:20 +0300

    square-n-sum

---
+++ 8kye/square-n-sum.js
@@ -0,0 +1,22 @@
+// DESCRIPTION (ОПИСАНИЕ)
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 
+
+// TESTS 
+// assert.strictEqual(squareSum([1,2]), 5);
+// assert.strictEqual(squareSum([0, 3, 4, 5]), 50);
+// assert.strictEqual(squareSum([]), 0);
+
+// SOLUTION (РЕШЕНИЕ)
+function squareSum(numbers){
+	let sum = 0
+	numbers.forEach(function(n) {
+		sum += n * n
+	});
+	return sum
+}
+
+console.log(squareSum([1,2]));
+
+
