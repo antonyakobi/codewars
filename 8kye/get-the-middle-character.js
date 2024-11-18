800ee3e8884612a086db7ac59f1ee9b6fb2d6f4b
antonyakobi <antonyakobi@yandex.by>
2024-11-18 23:55:48 +0300

    star

---
+++ 8kye/get-the-middle-character.js
@@ -0,0 +1,26 @@
+// DESCRIPTION (ОПИСАНИЕ)
+// You are going to be given a non-empty string. Your job is to return the middle character(s) of the string.
+// If the string's length is odd, return the middle character.
+// If the string's length is even, return the middle 2 characters.
+// Вам будет дана непустая строка. Ваша задача - вернуть средний символ (символы) строки.
+// Если длина строки нечетная, верните средний символ.
+// Если длина строки четная, верните 2 средних символа.
+
+// Examples:
+// "test" --> "es"
+// "testing" --> "t"
+// "middle" --> "dd"
+// "A" --> "A"
+
+// TESTS 
+// doTest("test", "es");
+// doTest("testing", "t");
+// doTest("middle", "dd");
+// doTest("A", "A");
+
+// SOLUTION (РЕШЕНИЕ)
+
+function getMiddle(s) {
+	//Code goes here!
+	return '';
+}
