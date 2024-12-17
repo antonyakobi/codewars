c090813a911413d13548eb044eaf3ed35c6cee2a
antonyakobi <antonyakobi@yandex.by>
2024-12-17 23:28:15 +0300

    7

--- 8kye/trainingJS#7if-else-and-ternary-operator.js
+++ 8kye/trainingJS#7if-else-and-ternary-operator.js
@@ -10,7 +10,7 @@
 // You can use if..else or ternary operator to complete it.
 // Для его завершения можно использовать оператор if..else или тернарный оператор.
 
-// TESTS 
+// TESTS
 // assert.strictEqual(saleHotdogs(  1),  100);
 // assert.strictEqual(saleHotdogs(  4),  400);
 // assert.strictEqual(saleHotdogs(  5),  475);
@@ -21,16 +21,22 @@
 // SOLUTION (РЕШЕНИЕ)
 
 function saleHotdogs(n) {
-	let cost = 0
-	
-	if (n < 5) {
-		cost = n * 100
-	} else if (n >= 5 && n < 10) {
-		cost = n * 95
-	} else {
-		cost = n * 90
-	}
-	return cost
+  let cost = 0;
+
+  if (n < 5) {
+    cost = n * 100;
+  } else if (n >= 5 && n < 10) {
+    cost = n * 95;
+  } else {
+    cost = n * 90;
+  }
+  return cost;
 }
+
+// ИЛИ
+
+// function saleHotdogs(n){
+//   return n * (n < 5 ? 100 : n < 10 ? 95 : 90);
+// }
 
 console.log(saleHotdogs(1));
