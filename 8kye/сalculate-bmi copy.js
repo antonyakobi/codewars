a97812761326b3bb4aeb82daad0a45b38784d145
antonyakobi <antonyakobi@yandex.by>
2025-01-05 23:56:57 +0300

    ascii-total

---
+++ 8kye/сalculate-bmi copy.js
@@ -0,0 +1,31 @@
+// You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all printable ASCII characters.
+// Вам будет дана строка, и вы должны вернуть сумму всех символов в виде int. Функция должна быть способна обрабатывать все печатаемые символы ASCII.
+
+// Examples:
+// uniTotal("a") == 97
+// uniTotal("aaa") == 291
+
+// TESTS 
+//   doTest("", 0);
+//   doTest("a", 97);
+//   doTest("b", 98);
+//   doTest("c", 99);
+//   doTest("d", 100);
+//   doTest("e", 101);
+//   doTest("aaa", 291);
+//   doTest("Mary Had A Little Lamb", 1873);
+
+// SOLUTION (РЕШЕНИЕ)
+
+function uniTotal (string) {
+	// total up dem unicodes!
+}
+
+console.log(uniTotal(""));
+console.log(uniTotal("a"));
+console.log(uniTotal("b"));
+console.log(uniTotal("c"));
+console.log(uniTotal("d"));
+console.log(uniTotal("e"));
+console.log(uniTotal("aaa"));
+console.log(uniTotal("Mary Had A Little Lamb"));
