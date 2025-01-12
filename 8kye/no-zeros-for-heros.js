d9efa6ad3d410b8cd2dbbbaf1b8ed029e4ae741d
antonyakobi <antonyakobi@yandex.by>
2025-01-12 23:46:00 +0300

    no-zeros-for-heros-start

---
+++ 8kye/no-zeros-for-heros.js
@@ -0,0 +1,38 @@
+// Numbers ending with zeros are boring.
+// They might be fun in your world, but not here.
+// Get rid of them. Only the ending ones
+
+// Числа, заканчивающиеся нулями, скучны. В вашем мире они могут быть забавными, но не здесь. Избавьтесь от них. Только конечные единицы
+
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+
+// Zero alone is fine, don't worry about it. Poor guy anyway
+// Ноль один - это хорошо, не беспокойтесь об этом. Бедный парень все равно
+
+// TESTS 
+// assert.strictEqual(noBoringZeros(1450), 145)
+// assert.strictEqual(noBoringZeros(960000), 96)
+// assert.strictEqual(noBoringZeros(1050), 105)
+// assert.strictEqual(noBoringZeros(-1050), -105)
+// assert.strictEqual(noBoringZeros(-105), -105)
+// assert.strictEqual(noBoringZeros(0), 0)
+
+// SOLUTION (РЕШЕНИЕ)
+
+function noBoringZeros(n) {
+    // your code
+}
+
+console.log(noBoringZeros(1450));
+console.log(noBoringZeros(960000));
+console.log(noBoringZeros(1050));
+console.log(noBoringZeros(-1050));
+console.log(noBoringZeros(-105));
+console.log(noBoringZeros(0));
+
+
+
+
